1169654e0a083b476c6acc2ab66e5b8ad395b6d5
Ken Thore <kenthore@live.no>
2022-11-11 11:40:20 +0100

    fixed API fetching

--- javascript/script.js
+++ javascript/script.js
@@ -1,10 +1,9 @@
-const url = "https://autotech.kenthore.no/wp-json/wp/v2/posts/";
-const mediaUrl = "https://autotech.kenthore.no/wp-json/wp/v2/media/";
+const url = "https://autotech.kenthore.no/wp-json/wp/v2/posts?per_page=20&_embed";
 const latestContainer = document.querySelector(".latest-big");
 const carouselContainer = document.querySelector(".carousel");
 const postsContainer = document.querySelector(".posts-slim");
 
- 
+//const mediaUrl = `https://autotech.kenthore.no/wp-json/wp/v2/posts/129?_embed`
 
 async function callAPI() {
     const response = await fetch(url);
@@ -12,11 +11,11 @@ async function callAPI() {
     
     //const mediaResponse = await fetch(mediaUrl);
     //const mediaResults = await mediaResponse.json();
-    
 
-    //renderLatest(results);
+    renderLatest(results);
     renderPosts(results);
 
+    
     
 };
 
@@ -27,12 +26,18 @@ function renderLatest(results) {
     latestContainer.innerHTML = "";
 
     latestContainer.innerHTML = `<h2 class="h2">Latest</h2>
-    <img class="header-big" src=""`;
+                                <a class="heading-card" href="post.html?id=${results[0].id}">
+                                <h2 class="h2">${results[0].title.rendered}</h2>
+                                <p class="post-author">${results[0].author}</p>
+                                <p class="post-date">${results[0].date}</p>
+                                <img class="header-big" src="${results[0]._embedded["wp:featuredmedia"][0].source_url}">
+                                <p>${results[0].excerpt.rendered}</p>
+                                </a>`;
 
 
 };
 
-renderLatest();
+
 
 function renderPosts(results) {
 
@@ -41,15 +46,13 @@ function renderPosts(results) {
     postsContainer.innerHTML = "";
 
     for (let i = 1; i < results.length; i++) {
-        postsContainer.innerHTML = `<a href="post.html?id=${results[i].id}">
+        postsContainer.innerHTML += `<a class="card-slim" href="post.html?id=${results[i].id}">
                                     <h2 class="h2">${results[i].title.rendered}</h2>
                                     <p class="post-author">${results[i].author}</p>
                                     <p class="post-date">${results[i].date}</p>
-    
+                                    <img class="header-big" src="${results[i]._embedded["wp:featuredmedia"][0].source_url}">
                                     <p>${results[i].excerpt.rendered}</p>
                                     </a>`;
     };
     postsContainer.innerHTML += `<a class="cta" href="posts-list.html">all posts</a>`;
 };
-
-renderPosts();
