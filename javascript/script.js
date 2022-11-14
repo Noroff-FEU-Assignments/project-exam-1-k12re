4c541853c83ec1109087f308dc5230f9520a8b11
Ken Thore <kenthore@live.no>
2022-11-15 00:20:44 +0100

    small css changes

--- javascript/script.js
+++ javascript/script.js
@@ -54,7 +54,7 @@ function renderPosts(results) {
     for (let i = 1; i < results.length; i++) {
         postsContainer.innerHTML += `<a class="card-slim" href="post.html?id=${results[i].id}">
                                     <h2 class="h2">${results[i].title.rendered}</h2>
-                                    <img class="header-big" src="${results[i]._embedded["wp:featuredmedia"][0].source_url}">
+                                    <img class="card-slim" src="${results[i]._embedded["wp:featuredmedia"][0].source_url}">
                                     <p class="post-author">${results[i]._embedded.author[0].name}</p>
                                     <p class="post-date">${results[i].date}</p>
                                     <p class="excerpt">${results[i].excerpt.rendered}</p>
