07e8ca49e863d5a5ba9b3207089c39baf9a2bc58
Ken Thore <kenthore@live.no>
2022-11-14 22:44:21 +0100

    some styling and small changes

--- javascript/script.js
+++ javascript/script.js
@@ -15,10 +15,10 @@ async function callAPI() {
     renderLatest(results);
     renderPosts(results);
 
-    results.forEach(thing => {
-        console.log(thing)
+    // results.forEach(thing => {
+    //     console.log(thing)
         
-    });
+    // });
     
     
 };
@@ -32,10 +32,10 @@ function renderLatest(results) {
     latestContainer.innerHTML = `<h2 class="h2">Latest</h2>
                                 <a class="heading-card" href="post.html?id=${results[0].id}">
                                 <h2 class="h2">${results[0].title.rendered}</h2>
-                                <p class="post-author">${results[0].author}</p>
-                                <p class="post-date">${results[0].date}</p>
                                 <img class="header-big" src="${results[0]._embedded["wp:featuredmedia"][0].source_url}">
-                                <p>${results[0].excerpt.rendered}</p>
+                                <p class="post-author">${results[0]._embedded.author[0].name}</p>
+                                <p class="post-date">${results[0].date}</p>
+                                <p class="excerpt">${results[0].excerpt.rendered}</p>
                                 </a>`;
 
 
@@ -54,10 +54,10 @@ function renderPosts(results) {
     for (let i = 1; i < results.length; i++) {
         postsContainer.innerHTML += `<a class="card-slim" href="post.html?id=${results[i].id}">
                                     <h2 class="h2">${results[i].title.rendered}</h2>
-                                    <p class="post-author">${results[i].author}</p>
-                                    <p class="post-date">${results[i].date}</p>
                                     <img class="header-big" src="${results[i]._embedded["wp:featuredmedia"][0].source_url}">
-                                    <p>${results[i].excerpt.rendered}</p>
+                                    <p class="post-author">${results[i]._embedded.author[0].name}</p>
+                                    <p class="post-date">${results[i].date}</p>
+                                    <p class="excerpt">${results[i].excerpt.rendered}</p>
                                     </a>`;
     };
     postsContainer.innerHTML += `<a class="cta" href="posts-list.html">all posts</a>`;
