64c2f0892fcba998cd91dabd4ed0268c3f3ae7ad
Ken Thore <kenthore@live.no>
2022-11-30 23:25:57 +0100

    testing form validation

--- javascript/contact.js
+++ javascript/contact.js
@@ -15,8 +15,9 @@ const textarea = document.querySelector(".textarea");
 const textareaReq = document.querySelector("#textarea-req");
 const textareaReqOk = document.querySelector(".textarea-req-ok");
 const contactForm = document.querySelector(".contact-form");
-const form = document.querySelector("form")
+const form = document.querySelector("form");
 const messageContainer = document.querySelector(".submission");
+const button = document.querySelector("button").disabled = true;
 
 async function callContact() {
     try {
@@ -41,64 +42,80 @@ function checkMail(email) {
     return mailPattern;
 };
 
-function validateInput(event) {
+// function validateInput(event) {
 
-    event.preventDefault();
+//     event.preventDefault();
 
-    if(name.value.length > 1) {
-        nameReq.style.display = "none";
-    } else {
-        nameReq.style.display = "block";
-    };
+//     if(name.value.length > 1) {
+//         nameReq.style.display = "none";
+//     } else {
+//         nameReq.style.display = "block";
+//     };
 
-    if(checkMail(email.value) === true) {
-        emailReq.style.display = "none";
-    } else {
-        emailReq.style.display = "block";
-    };
+//     if(checkMail(email.value) === true) {
+//         emailReq.style.display = "none";
+//     } else {
+//         emailReq.style.display = "block";
+//     };
 
-    if(subject.value.length > 7) {
-        subjectReq.style.display = "none";
-    } else {
-        subjectReq.style.display = "block";
-    }
+//     if(subject.value.length > 7) {
+//         subjectReq.style.display = "none";
+//     } else {
+//         subjectReq.style.display = "block";
+//     }
 
-    if(textarea.value.length > 24) {
-        textareaReq.classList.add("req");
-    } else {
-        textareaReq.classList.remove("req");
-    }
-};
+//     if(textarea.value.length > 24) {
+//         textareaReq.classList.add("req");
+//     } else {
+//         textareaReq.classList.remove("req");
+//     }
+// };
 
 name.onkeyup = function() {
     if(name.value.length > 1) {
-        nameReqOk.style.display = "block"
+        nameReqOk.style.display = "block";
+        nameReq.style.display = "none";
+        button.disabled = false;
     } else {
-        nameReqOk.style.display = "none"
+        nameReqOk.style.display = "none";
+        nameReq.style.display = "block";
+        
     };
 };
 
 email.onkeyup = function() {
     if(checkMail(email.value)) {
-        emailReqOk.style.display = "block"
+        emailReqOk.style.display = "block";
+        emailReq.style.display = "none";
+        button.disabled = false;
     } else {
-        emailReqOk.style.display = "none"
+        emailReqOk.style.display = "none";
+        emailReq.style.display = "block";
+        
     };
 };
 
 subject.onkeyup = function() {
     if(subject.value.length > 7) {
-        subjectReqOk.style.display = "block"
+        subjectReqOk.style.display = "block";
+        subjectReq.style.display = "none";
+        button.disabled = false;
     } else {
-        subjectReqOk.style.display = "none"
+        subjectReqOk.style.display = "none";
+        subjectReq.style.display = "block";
+        
     };
 };
 
 textarea.onkeyup = function() {
     if(textarea.value.length > 24) {
-        textareaReqOk.style.display = "block"
+        textareaReqOk.style.display = "block";
+        textareaReq.classList.add("req");
+        button.disabled = false;
     } else {
-        textareaReqOk.style.display = "none"
+        textareaReqOk.style.display = "none";
+        textareaReq.classList.remove("req");
+        
     };
 };
 
@@ -110,10 +127,15 @@ function submitForm(event) {
     event.preventDefault();
  
     if (checkInputs(name.value, 1) && (email.value, true) && (subject.value, 8) && textarea.value, 25) {
+        button.disabled = false;
         messageContainer.innerHTML = `<div class="message">Your message has been sent<div>`;
+        nameReqOk.style.display = "none";
+        emailReqOk.style.display = "none";
+        subjectReqOk.style.display = "none";
+        textareaReqOk.style.display = "none";
         form.reset();
     } else {
-        messageContainer.innerHTML = `<div class="error">Please insert valid info<div>`;
+        button.disabled = true;
     };
 };
 
@@ -125,5 +147,8 @@ function checkInputs(value, length) {
     }
 };
 
-contactForm.addEventListener("submit", validateInput);
+// contactForm.addEventListener("submit", validateInput);
 contactForm.addEventListener("submit", submitForm);
+
+
+console.log(button.disabled);
